7df8ade0a9a9cf250d16ad0d5ed469764edbe090
Mk <markantkelly93@gmail.com>
2021-12-04 20:28:17 +0000

    Day 1 complete

--- day1/part1.go
+++ day1/part1.go
@@ -3,14 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	d := depthsTest()
+	//Part1
+	d1 := depths()
 	lastDepth := 0
 	increases := 0
-	for i := 0; i < len(d); i++ {
-		if i != 0 && d[i] > lastDepth {
+
+	for i := 0; i < len(d1); i++ {
+		if i != 0 && d1[i] > lastDepth {
 			increases++
 		}
-		lastDepth = d[i]
+		lastDepth = d1[i]
 	}
 	fmt.Println(increases)
+	//Part 2
+	d2 := depths()
+	SlidingDepth := 0
+	SlidingInc := 0
+
+	for i := 0; i+2 < len(d2); i++ {
+		if i != 0 && d2[i]+d2[i+1]+d2[i+2] > SlidingDepth {
+			SlidingInc++
+		}
+		SlidingDepth = d2[i] + d2[i+1] + d2[i+2]
+	}
+
+	fmt.Println(SlidingInc)
 }
