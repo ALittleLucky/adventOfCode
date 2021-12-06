8c6e3d90f155cec2748de074facf81024b97ed41
Mk <markantkelly93@gmail.com>
2021-12-06 06:54:25 +0000

    cleaner solution

--- day6/day6.go
+++ day6/day6.go
@@ -5,27 +5,20 @@ import "fmt"
 func main() {
 	fish := LoadFish("data.txt")
 	simulationLength := 80
-	fmt.Println("problem 1", simulate(fish, simulationLength))
+	fmt.Println("problem 1:", simulate(fish, simulationLength))
 
 	simulationLength = 256
-	fmt.Println("problem 2", simulate(fish, simulationLength))
+	fmt.Println("problem 2:", simulate(fish, simulationLength))
 }
 
 func simulate(fish [9]int, l int) int {
-	newfish := fish
+	var newfish [9]int
 	for s := 0; s < l; s++ {
-
-		for i := len(fish) - 1; i >= 0; i-- {
-			if i == 8 {
-				newfish[i] = fish[0] // add new fish
-			} else if i == 0 {
-				newfish[6] = fish[i] + fish[7] // loop fish
-				newfish[0] = fish[i+1]
-			} else {
-				newfish[i] = fish[i+1]
-			}
+		fish[7] = fish[7] + fish[0]
+		newfish[8] = fish[0]
+		for i := len(fish) - 2; i >= 0; i-- {
+			newfish[i] = fish[i+1]
 		}
-
 		fish = newfish
 	}
 	count := 0
